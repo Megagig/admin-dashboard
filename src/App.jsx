804de57ec6c2f6080e2d7e9f3408b82669df92c9
Megagig <megagigsoftwaresolutions.com>
2024-06-26 20:58:57 +0100

    Add the PublicRoutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import Router from './router/Router';
+import publicRoutes from './router/routes/publicRoutes';
 
 function App() {
-  const [allRoutes, setAllroutes] = useState([]);
+  const [allRoutes, setAllroutes] = useState([...publicRoutes]);
+
   return <Router allRoutes={allRoutes} />;
 }
 

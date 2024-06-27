3b2c98776049e1c0289a02d5a56541d7f4364feb
Megagig <megagigsoftwaresolutions.com>
2024-06-27 15:28:48 +0100

    Add the Register form

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -2,8 +2,28 @@ import React from 'react';
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
+    <div className="min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center">
+      <div className="w-[350px] text-[#ffffff] p-2">
+        <div className="bg-[#6f68d1] p-4 rounded-md">
+          <h2 className="text-xl mb-3 font-bold "> Welcome to MegaShop</h2>
+          <p className="text-sm mb-3 font-medium">
+            Please Register Your Account
+          </p>
+          <form>
+            <div className="flex flex-col w-full gap-1 mb-3">
+              <label htmlFor="name">Name</label>
+              <input
+                className="px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md"
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Name"
+                required
+              />
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };

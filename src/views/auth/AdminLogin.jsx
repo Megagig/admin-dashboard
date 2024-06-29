dcd884d998b88401959ed3820b8f713ec3d21944
Megagig <megagigsoftwaresolutions.com>
2024-06-29 22:06:33 +0100

    Finish the Design of the Admin Component with Tailwind

--- src/views/auth/AdminLogin.jsx
+++ src/views/auth/AdminLogin.jsx
@@ -1,9 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaGoogle } from 'react-icons/fa';
+import { FaFacebook } from 'react-icons/fa';
 
 const AdminLogin = () => {
+  const [state, setState] = useState({
+    email: '',
+    password: '',
+  });
+
+  const inputHandle = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(state);
+  };
   return (
-    <div>
-      <h1>Admin Login</h1>
+    <div className="min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center">
+      <div className="w-[350px] text-[#ffffff] p-2">
+        <div className="bg-[#6f68d1] p-4 rounded-md">
+          <div className="flex h-[70px] justify-center items-center">
+            <div className="w-[180px] h-[50px]">
+              <img
+                className="w-full h-full"
+                src="http://localhost:3000/images/logo.png"
+                alt="logo"
+              />
+            </div>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col w-full gap-1 mb-3">
+              <label htmlFor="name">Email</label>
+              <input
+                onChange={inputHandle}
+                value={state.email}
+                className="px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md"
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email"
+                required
+              />
+            </div>
+            <div className="flex flex-col w-full gap-1 mb-3">
+              <label htmlFor="name">Password</label>
+              <input
+                onChange={inputHandle}
+                value={state.password}
+                className="px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md"
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+                required
+              />
+            </div>
+
+            <button className="bg-slate-800 w-full hover: shadow-blue-300/hover: shawdow-lg text-white rounded-md px-7 py-2 mb-3">
+              Login
+            </button>
+
+            <div className="flex w-full items-center mb-3 justify-center">
+              <div className="w-[45%] bg-slate-700 h-[1px]"></div>
+              <div className="w-[10%] flex items-center justify-center">
+                <span className="pb-1">OR</span>
+              </div>
+              <div className="w-[45%] bg-slate-700 h-[1px]"></div>
+            </div>
+            <div className="flex justify-center items-center gap-3">
+              <div className="w-[135px] h-[35px] flex rounded-md bg-orange-700 shadow-lg hover:shadow-orange-700/50 justify-center cursor-pointer items-center overflow-hidden">
+                <span>
+                  <FaGoogle />
+                </span>
+              </div>
+              <div className="w-[135px] h-[35px] flex rounded-md bg-blue-700 shadow-lg hover:shadow-blue-700/50 justify-center cursor-pointer items-center overflow-hidden">
+                <span>
+                  <FaFacebook />
+                </span>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };

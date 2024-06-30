b0ef0be1a1bd7aa4564e1a436f5a740063fa18e6
Megagig <megagigsoftwaresolutions.com>
2024-06-30 15:45:40 +0100

    Imported and configured Toaster from react-hot-toast

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { Toaster } from 'react-hot-toast';
 import store from './store/index';
 const App = lazy(() => import('./App'));
 
@@ -13,6 +14,15 @@ root.render(
     <Provider store={store}>
       <Suspense>
         <App />
+        <Toaster
+          toastOptions={{
+            position: 'top-right',
+            style: {
+              background: '#283046',
+              color: '#fff',
+            },
+          }}
+        />
       </Suspense>
     </Provider>
   </BrowserRouter>

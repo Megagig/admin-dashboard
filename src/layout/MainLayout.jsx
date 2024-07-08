65bbc0210786870090693e1c2a2b967457738bb8
Megagig <megagigsoftwaresolutions.com>
2024-07-08 13:27:15 +0100

    Design the MainLayout structure

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Sidebar from './Sidebar';
 
 const MainLayout = () => {
   return (
-    <div>
+    <div className="bg-[#cdcae9] w-full min-h-screen">
       <h1>
-        Main Layout <Outlet />
+        <Header />
+        <Sidebar />
+        <div className="ml-0 lg:ml-[260px] pt-[95px] transition-all">
+          <Outlet />
+        </div>
       </h1>
     </div>
   );

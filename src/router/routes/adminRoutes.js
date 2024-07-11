5c7521e80620352c7ddcb3de899159dccb410a8d
Megagig <megagigsoftwaresolutions.com>
2024-07-11 18:24:46 +0100

    Add Seller Details Page

--- src/router/routes/adminRoutes.js
+++ src/router/routes/adminRoutes.js
@@ -1,4 +1,5 @@
 import { lazy } from 'react';
+
 const AdminDashboard = lazy(() => import('../../views/admin/AdminDashboard'));
 const Orders = lazy(() => import('../../views/admin/Orders'));
 const Category = lazy(() => import('../../views/admin/Category'));
@@ -7,6 +8,7 @@ const PaymentRequest = lazy(() => import('../../views/admin/PaymentRequest'));
 const DeactivateSellers = lazy(() =>
   import('../../views/admin/DeactivateSellers')
 );
+const SellerRequest = lazy(() => import('../../views/admin/SellerRequest'));
 
 export const adminRoutes = [
   {
@@ -37,6 +39,11 @@ export const adminRoutes = [
   {
     path: 'admin/dashboard/deactivate-sellers',
     element: <DeactivateSellers />,
+    role: 'admin',
+  },
+  {
+    path: 'admin/dashboard/sellers-request',
+    element: <SellerRequest />,
     role: 'admin',
   },
 ];

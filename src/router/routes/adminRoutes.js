1945e07411f88a1a89dc2c7f5ba446c20288d704
Megagig <megagigsoftwaresolutions.com>
2024-07-11 17:29:21 +0100

    Finished the Deactivate Seller Section of the Dashboard

--- src/router/routes/adminRoutes.js
+++ src/router/routes/adminRoutes.js
@@ -1,10 +1,12 @@
 import { lazy } from 'react';
-
 const AdminDashboard = lazy(() => import('../../views/admin/AdminDashboard'));
 const Orders = lazy(() => import('../../views/admin/Orders'));
 const Category = lazy(() => import('../../views/admin/Category'));
 const Sellers = lazy(() => import('../../views/admin/Sellers'));
 const PaymentRequest = lazy(() => import('../../views/admin/PaymentRequest'));
+const DeactivateSellers = lazy(() =>
+  import('../../views/admin/DeactivateSellers')
+);
 
 export const adminRoutes = [
   {
@@ -30,6 +32,11 @@ export const adminRoutes = [
   {
     path: 'admin/dashboard/payment-request',
     element: <PaymentRequest />,
+    role: 'admin',
+  },
+  {
+    path: 'admin/dashboard/deactivate-sellers',
+    element: <DeactivateSellers />,
     role: 'admin',
   },
 ];
